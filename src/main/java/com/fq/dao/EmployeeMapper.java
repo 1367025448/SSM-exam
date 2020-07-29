bdbfc0c178ada4755aeba0f95331168dc969c921
fq_pro <1367025448@qq.com>
2020-07-29 19:48:46 +0800

    first

---
+++ src/main/java/com/fq/dao/EmployeeMapper.java
@@ -0,0 +1,16 @@
+package com.fq.dao;
+
+import com.fq.domain.Employee;
+import java.util.List;
+
+public interface EmployeeMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Employee record);
+
+    Employee selectByPrimaryKey(Integer id);
+
+    List<Employee> selectAll();
+
+    int updateByPrimaryKey(Employee record);
+}
